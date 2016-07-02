70445e7cb112d5debb075167e4b37ecd6aaa01a5
xanio <rangsan_m@hotmail.com>
2016-07-02 11:42:52 +0700

    Beta

---
+++ connection.h
@@ -0,0 +1,48 @@
+void connectionWIFI(char* WLAN_SSID,char* WLAN_PASS){
+    Serial.print("Wifi Connecting to SSD:");
+    Serial.println(WLAN_SSID);
+    WiFi.begin(WLAN_SSID, WLAN_PASS);                                                   //เชื่อมต่อกับ AP
+    Serial.print("MAC adress:");
+    Serial.println(WiFi.macAddress());
+    int count =0; 
+    
+    while (WiFi.status() != WL_CONNECTED)                                     //รอการเชื่อมต่อ
+    {
+          delay(500);
+          Serial.print(".");
+          ++count;
+          if(count >5000){
+            ESP.reset();
+            return;
+          }
+    }
+    Serial.println("");
+    Serial.print("WiFi connected IP:");                                         //แสดงข้อความเชื่อมต่อสำเร็จ  
+    Serial.println(WiFi.localIP());                                           // แสดงหมายเลข IP ของ Server    
+    Serial.println("<<  <<< << Network READY >>>>> >>>  >>");
+}
+
+void thingsSpeak(float temperature,float humitdity) {
+  
+    static const char*      HOST_CLOUD   = "api.thingspeak.com";
+    static const char*      apiKEY_CLOUD = "OJDJI20NWYIRFWWB";
+    WiFiClient client_1;                                  // Use WiFiClient class to create TCP connections
+
+    if (!client_1.connect(HOST_CLOUD, 80)) {
+         Serial.print(HOST_CLOUD);
+         Serial.println("connection failed");
+        return;
+    }
+    String url = "/update/";                            // We now create a URI for the request
+    url += "?key=";                                     //  url += streamId;
+    url += apiKEY_CLOUD;
+    url += "&field1=";
+    url += temperature;
+    url += "&field2=";
+    url += humitdity;              
+    client_1.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + HOST_CLOUD + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    Serial.println("thingSpeak.com =>Send!! success");
+}
+
--- farmmingSystem_01.ino
+++ farmmingSystem_01.ino
@@ -1,37 +1,143 @@
 /* Design Xanio
  * Create 27/06/2016
- * Sensor  -LDR                   - DS3231
+ * Sensor  -LDR                   - DS3231 
  *         -DHT11                 - ESP8266
- *         -Soil Moisture         
+ *         -Soil Moisture         - ADS1115
 */
-#include <Wire.h>  // must be incuded here so that Arduino library object file references work
+
+#include <Wire.h>  
 #include <RtcDS3231.h>
 #include <Time.h>
 #include <Adafruit_GFX.h>
 #include <ESP_Adafruit_SSD1306.h>
+#include "ESP8266WiFi.h"
+#include "PubSubClient.h"
+#include "DHT.h"
+#include "connection.h"
+#include "ArduinoJson.h"
+#include "TimeInterval.h"
+#include "LED.h"
 
-RtcDS3231           Rtc;
+
+/*******************************************
+ * Configgulation
+ *******************************************/
+
+#define                 WLAN_SSID       "no_angel"
+#define                 WLAN_PASS       "o8o9237579"
+
+#define                 MQTT_USER       "controller_01"
+#define                 MQTT_PASSWORD   "123456789"
+#define                 TOPIC_SUB       "DEVICE/01/command/"
+#define                 TOPIC_PUB       "DEVICE/01/status/"
+
+/********************************************
+ *        Define Sensor & Module
+ *********************************************/
+#define             DHTPIN D4                         // Sensor DHT11
 #define             OLED_RESET 4  
-Adafruit_SSD1306    display(OLED_RESET);
 #define countof(a) (sizeof(a) / sizeof(a[0]))
+
+DHT                 dht(DHTPIN, DHT11);
+RtcDS3231           Rtc;
+Adafruit_SSD1306    display(OLED_RESET);
+
+WiFiClient              wclient;
+PubSubClient            mqttClient(wclient);
+
+LED                     relay_0(D5);
+LED                     relay_1(D6);
+LED                     relay_2(D7);
+LED                     relay_3(D8);
+
+TimeInterval            timeToPublish(15000);     // ส่ง MQTT ทุกๆๆ ค่าเวลาที่เรากำหนด
 
 void setup() {
   Serial.begin(115200);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);  //initialize I2C addr 0x3c
-  display.clearDisplay();                     // clears the screen and buffer
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);  
+  display.clearDisplay();                     
   display.drawPixel(127, 63, WHITE);
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.display();
 
+  Serial.println("Start Farmimg..");
+  connectionWIFI(WLAN_SSID,WLAN_PASS);
+  
+  mqttClient.setServer("m11.cloudmqtt.com", 15507);             //->Connection to MQTT Cloud.
+  mqttClient.setCallback(callback);
+  MQTTConnect();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  mqttClient.loop(); 
+  MQTTConnect();                                                // Mqtt รออ่านข้อมูลจาก CloudMQTT
+  if(timeToPublish.every(15000)){
+    DataToCloud();
+  }
 }
 
-void setInterval(long milisec){
+void MQTTConnect() {
   
+  if (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (mqttClient.connect("ESP8266_MQTT", MQTT_USER, MQTT_PASSWORD)) {         // Attempt to connect
+      Serial.println("cloudmqtt connected ");                                  // Once connected, publish an announcement..
+      mqttClient.subscribe(TOPIC_SUB);                                         // อ่านหัวข้อ Topic
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+    }
+  }
+}
+
+/*****************************************************************
+ *  Function callBack รอคำสั่งจาก MQTT Cloud ในรูปแบบ JSON 
+ *****************************************************************/
+void callback(char* topic, byte* payload, unsigned int length){
+  String message="";
+  Serial.print("MQTT Read      =>");
+  Serial.print(topic);
+  Serial.print("=> ");
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.println(message);
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(message);
+  
+  String device = root["deviceID"];
+  if(device == "0001"){
+    relay_0.set((int)root["relay_0"]);
+    relay_1.set((int)root["relay_1"]);
+    relay_2.set((int)root["relay_2"]);
+    relay_3.set((int)root["relay_3"]);
+    DataToCloud();                                         // Return status Data ทั้งหมดขึ้นสู่ Cloud
+  }
+}
+/******************************************************
+ *  function ส่งค่าต่างจาก sensor  ขึ้น cloud
+ ******************************************************/
+void DataToCloud(){
+  float humidity        = dht.readHumidity();             // Sensor วัดความชื้นและอุณหภูมิ
+  float temperature     = dht.readTemperature();
+  
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("!! ERROR DHT11 can't read data !!");
+    timeToPublish.reset();     
+  } else {
+    String dataJson = "{\"temp\":\""+(String)temperature+"\",\"Hum\":\""+(String)humidity+"\",\"relay_0\":"+relay_0.readStatusLED()
+                      +",\"relay_1\":"+relay_1.readStatusLED()
+                      +",\"relay_2\":"+relay_2.readStatusLED()
+                      +",\"relay_3\":"+relay_3.readStatusLED()+"}";
+    thingsSpeak(temperature,humidity);
+    char charBuf[200];
+    dataJson.toCharArray(charBuf,200);
+    mqttClient.publish(TOPIC_PUB,charBuf );
+    Serial.print("MQTT Send !    =>");
+    Serial.print("Topic:"+(String)TOPIC_PUB);
+    Serial.println(charBuf);
+  }
 }
 
